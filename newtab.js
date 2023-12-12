4fe9e52895af856f01511ab1f0c6de49d2b0ce23
nyannncatt <kamirelax187@gmail.com>
2023-12-13 01:31:40 +0800

    added another big changes for contents

--- newtab.js
+++ newtab.js
@@ -15,17 +15,51 @@ function openfb() {
 
 
 
-  function openfb() {
+  function openfb2() {
     var url = "https://www.facebook.com/lovelainejoy.tasic.3"
     window.open(url, '_blank', 'noopener,noreferrer');
   }
  
-  function opengit() {
+  function opengit2() {
     var url = "https://github.com/LOVELAINEJOYTASIC"
     window.open(url, '_blank', 'noopener,noreferrer');
   }
-  function openyt() {
+  function openyt2() {
     var url = "https://www.youtube.com/@eljouy1743"
     window.open(url, '_blank', 'noopener,noreferrer');
   }
+
+
+
+
+  function openrepo() {
+    var url = "https://github.com/nyannncatt/Elective"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
  
+  function openrepo2() {
+    var url = "https://github.com/nyannncatt/hover-link-animation"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openrepo3() {
+    var url = "https://github.com/nyannncatt/Inventorysimple"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+
+
+  function openrepo4() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/spa-jquery"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function openrepo5() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/Bouncing-Ball-Act"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/-My-Portfolio"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+
