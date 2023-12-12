fd9db9e138f006e58a9da4237ff20ef17b2ac529
nyannncatt <kamirelax187@gmail.com>
2023-12-12 14:28:32 +0800

    minor changes

---
+++ newtab.js
@@ -0,0 +1,31 @@
+function openfb() {
+    var url = "https://www.facebook.com/EzLawliet"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function opengit() {
+    var url = "https://github.com/nyannncatt"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openyt() {
+    var url = "https://www.youtube.com/channel/UCDCwtfzgDeQruL0tCNLKdkw"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+
+
+
+  function openfb() {
+    var url = "https://www.facebook.com/lovelainejoy.tasic.3"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function opengit() {
+    var url = "https://github.com/LOVELAINEJOYTASIC"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openyt() {
+    var url = "https://www.youtube.com/@eljouy1743"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
